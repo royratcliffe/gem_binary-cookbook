4d58b3f55f9303aab2a3104674ce0aef7723d0a7
Roy Ratcliffe <roy@pioneeringsoftware.co.uk>
2012-11-08 06:12:43 +0000

    Runs a ruby_block for setting up gem_binary for all gem_package resources
    Cookbook from template.

---
+++ CHANGELOG.md
@@ -0,0 +1,12 @@
+# CHANGELOG for gem_binary
+
+This file is used to list changes made in each version of gem_binary.
+
+## 0.1.0:
+
+* Initial release of gem_binary
+
+- - - 
+Check the [Markdown Syntax Guide](http://daringfireball.net/projects/markdown/syntax) for help with Markdown.
+
+The [Github Flavored Markdown page](http://github.github.com/github-flavored-markdown/) describes the differences between markdown on github and standard markdown.
---
+++ recipes/default.rb
@@ -0,0 +1,22 @@
+#
+# Cookbook Name:: gem_binary
+# Recipe:: default
+#
+# Copyright 2012, YOUR_COMPANY_NAME
+#
+# All rights reserved - Do Not Redistribute
+#
+
+# Iterate through all the gem_package resources changing the Gem binary to
+# node[:gem_binary] unless already configured. Do this at the end of
+# compilation, just before convergence when the gem_package resources perform
+# their install action.
+ruby_block "gem_binary" do
+  block do
+    run_context.resource_collection.select do |resource|
+      resource.resource_name == :gem_package
+    end.each do |resource|
+      resource.gem_binary node[:gem_binary] unless resource.gem_binary
+    end
+  end
+end
